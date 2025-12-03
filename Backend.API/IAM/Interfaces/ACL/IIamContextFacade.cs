5ecbe8c1cdcf78572103333bdce18402f0fa1d59
User <u202417693@upc.edu.pe>
2025-12-03 05:34:39 -0500

    feat: implement user authentication and authorization features

---
+++ Backend.API/IAM/Interfaces/ACL/IIamContextFacade.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.API.IAM.Interfaces.ACL;
+
+public interface IIamContextFacade
+{
+    Task<int> CreateUser(string username, string password);
+    Task<int> FetchUserIdByUsername(string username);
+    Task<string> FetchUsernameByUserId(int userId);
+}
